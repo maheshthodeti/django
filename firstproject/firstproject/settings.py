1fb713c73562345436ca0859a926a7c40a16732c
maheshthodeti <130722309+maheshthodeti@users.noreply.github.com>
2023-04-25 12:14:55 +0530

    Update settings.py

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -25,9 +25,8 @@ SECRET_KEY = 'django-insecure-_g&fs+a2ajceronh!$y(duh4jb(ffj2p*o*zcib)7g22lzzom@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = ['MAHESH.pythonwhere.com']
+ALLOWED_HOSTS = ['maheshthhodti.pythonwhere.com']
 # Application definition
 
 INSTALLED_APPS = [
